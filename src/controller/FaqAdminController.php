65ba0197b677da2ccf03adb2fed08644ca6fb905
Flotss <manginf54@gmail.com>
2024-04-15 20:23:03 +0200

    Add FaqAdminController and FaqController classes for FAQ functionality

---
+++ src/controller/FaqAdminController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+class FaqAdminController extends AbstractController
+{
+
+    public function __construct()
+    {
+        parent::__construct("faq/admin");
+    }
+}
---
+++ src/controller/FaqController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+class FaqController extends AbstractController
+{
+
+    public function __construct()
+    {
+        parent::__construct("faq/user");
+    }
+}
