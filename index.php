6998e2edf7a96793485c7d064f86ad2ddec329d1
Flotss <manginf54@gmail.com>
2024-04-15 12:06:12 +0200

    Refactor index.php and add Router, Route, RouterException classes in src/ directory

--- index.php
+++ index.php
@@ -11,10 +11,29 @@
     <?php
     require_once 'vendor/autoload.php';
 
+    use App\Router\Router;
+    use App\Router\Route;
     use App\Controller\IndexController;
 
-    $controller = new IndexController();
-    $controller->index();
+
+    // TODO : MAKE DYNAMIC HEADER 
+    // HEADER IS NOT SAME IF WE ARE IN ADMIN PAGE OR USER PAGE
+
+
+    $router = new Router($_GET['url']);
+    echo $router->url;
+    $router->get('/', function () {
+        $controller = new IndexController();
+        $controller->index();
+    });
+
+    $router->run();
+
+
+    // TODO : CREATE FOOTER
+    // AND RUN IT HERE
+
+
     ?>
 </body>
 
---
+++ src/Exceptions/RouterException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Exceptions;
+
+class RouterException extends \Exception
+{
+}
