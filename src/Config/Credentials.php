436224c8803e66b25281d86fd7e6d26b64325a46
Flotss <manginf54@gmail.com>
2024-04-16 23:41:36 +0200

    Add Credentials class for database configuration

---
+++ src/Config/Credentials.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Config;
+
+class Credentials
+{
+    const DB_HOST = 'localhost';
+    const DB_NAME = 'database_name';
+    const DB_USER = 'database_user';
+    const DB_PASSWORD = 'database_password';
+}
