d080bdc1456f83286b9ea01a7da4a193af1d38a6
Flotss <manginf54@gmail.com>
2024-04-17 01:54:55 +0200

    Add documentation to DataBaseSingleton

--- src/Database/DataBaseSingleton.php
+++ src/Database/DataBaseSingleton.php
@@ -6,12 +6,17 @@ use App\Config\Credentials;
 use App\Entity\User;
 use PDO;
 
-
+/**
+ * The DataBaseSingleton class represents a singleton database connection.
+ */
 class DataBaseSingleton
 {
     private static ?DataBaseSingleton $instance = null;
     private PDO $connection;
 
+    /**
+     * Private constructor to prevent creating multiple instances of the class.
+     */
     private function __construct()
     {
         $credentials = Credentials::getCredentials(Credentials::DB_DEV);
@@ -23,6 +28,11 @@ class DataBaseSingleton
         );
     }
 
+    /**
+     * Get the instance of the DataBaseSingleton class.
+     *
+     * @return DataBaseSingleton The instance of the DataBaseSingleton class.
+     */
     public static function getInstance(): DataBaseSingleton
     {
         if (self::$instance === null) {
@@ -31,12 +41,22 @@ class DataBaseSingleton
         return self::$instance;
     }
 
+    /**
+     * Get the database connection.
+     *
+     * @return PDO The database connection.
+     */
     public function getConnection(): PDO
     {
         return $this->connection;
     }
 
-
+    /**
+     * Save a user to the database.
+     *
+     * @param User $user The user to be saved.
+     * @return void
+     */
     public function saveUser(User $user)
     {
         $query = $this->connection->prepare('INSERT INTO users (username, email, password) VALUES (:username, :email, :password)');
@@ -47,6 +67,12 @@ class DataBaseSingleton
         ]);
     }
 
+    /**
+     * Get a user from the database by username.
+     *
+     * @param string $username The username of the user.
+     * @return User|null The user object if found, null otherwise.
+     */
     public function getUserByUsername(string $username): ?User
     {
         $query = $this->connection->prepare('SELECT * FROM users WHERE username = :username');
@@ -60,6 +86,12 @@ class DataBaseSingleton
         return null;
     }
 
+    /**
+     * Get a user from the database by email.
+     *
+     * @param string $email The email of the user.
+     * @return User|null The user object if found, null otherwise.
+     */
     public function getUserByEmail(string $email): ?User
     {
         $query = $this->connection->prepare('SELECT * FROM users WHERE email = :email');
@@ -73,6 +105,13 @@ class DataBaseSingleton
         return null;
     }
 
+    /**
+     * Make a database query and return the result as an array.
+     *
+     * @param string $query The SQL query.
+     * @param array $params The parameters for the query.
+     * @return array The result of the query as an array.
+     */
     public function makeRequest($query, $params = []): array
     {
         $query = $this->connection->prepare($query);
