cd2e6a9a8e7abb52e3390c938309807f486f1de5
Flotss <manginf54@gmail.com>
2024-04-17 19:39:01 +0200

    Add docs

--- src/Config/Credentials.php
+++ src/Config/Credentials.php
@@ -4,11 +4,32 @@ namespace App\Config;
 
 class Credentials
 {
+    /**
+     * The production database environment.
+     *
+     * @var string
+     */
     public static $DB_PROD = 'prod';
+
+    /**
+     * The development database environment.
+     *
+     * @var string
+     */
     public static $DB_DEV = 'dev';
 
+    /**
+     * The array to store the database credentials.
+     *
+     * @var array
+     */
     private static $credentials = [];
 
+    /**
+     * Initializes the database credentials.
+     *
+     * @return void
+     */
     public static function init()
     {
         self::$credentials[self::$DB_PROD] =
@@ -17,6 +38,12 @@ class Credentials
             new DataBaseCredentials('mysql-4d9b6a3-eventit.b.aivencloud.com', 'defaultdb', 15997, 'avnadmin', 'AVNS_9GXsMZrg9tnGk2_opAu');
     }
 
+    /**
+     * Retrieves the database credentials for the specified environment.
+     *
+     * @param string $env The environment (prod or dev).
+     * @return DataBaseCredentials|null The database credentials or null if not found.
+     */
     public static function getCredentials($env)
     {
         return isset(self::$credentials[$env]) ? self::$credentials[$env] : null;
--- src/Config/DatabaseCredentials.php
+++ src/Config/DatabaseCredentials.php
@@ -2,14 +2,45 @@
 
 namespace App\Config;
 
+/**
+ * Class DataBaseCredentials
+ * Represents the credentials required to connect to a database.
+ */
 class DataBaseCredentials
 {
+    /**
+     * @var string The host of the database.
+     */
     private $host = 'localhost';
+
+    /**
+     * @var string The name of the database.
+     */
     private $name = 'database_name';
+
+    /**
+     * @var int The port number of the database.
+     */
     private $port = 3306;
+
+    /**
+     * @var string The username for the database connection.
+     */
     private $user = 'database_user';
+
+    /**
+     * @var string The password for the database connection.
+     */
     private $password = 'database_password';
 
+    /**
+     * DataBaseCredentials constructor.
+     * @param string $host The host of the database.
+     * @param string $name The name of the database.
+     * @param int $port The port number of the database.
+     * @param string $user The username for the database connection.
+     * @param string $password The password for the database connection.
+     */
     public function __construct($host, $name, $port, $user, $password)
     {
         $this->host = $host;
@@ -19,26 +50,46 @@ class DataBaseCredentials
         $this->password = $password;
     }
 
+    /**
+     * Get the host of the database.
+     * @return string The host of the database.
+     */
     public function getHost()
     {
         return $this->host;
     }
 
+    /**
+     * Get the name of the database.
+     * @return string The name of the database.
+     */
     public function getName()
     {
         return $this->name;
     }
 
+    /**
+     * Get the port number of the database.
+     * @return int The port number of the database.
+     */
     public function getPort()
     {
         return $this->port;
     }
 
+    /**
+     * Get the username for the database connection.
+     * @return string The username for the database connection.
+     */
     public function getUser()
     {
         return $this->user;
     }
 
+    /**
+     * Get the password for the database connection.
+     * @return string The password for the database connection.
+     */
     public function getPassword()
     {
         return $this->password;
