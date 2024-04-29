e47904b0049d5b1a1949eb2a1473e9ee129b9239
Flotss <manginf54@gmail.com>
2024-04-29 23:21:52 +0200

    add data to FAQController and make page for FAQ

--- src/controller/FaqController.php
+++ src/controller/FaqController.php
@@ -8,5 +8,12 @@ class FaqController extends AbstractController
     public function __construct()
     {
         parent::__construct("faq/user");
+
+        $questions = [];
+        $questions[0] = ["question" => "What is the purpose of this website?", "answer" => "This website is a platform for students to share their notes and study materials with each other.This website is a platform for students to share their notes and study materials with each other.This website is a platform for students to share their notes and study materials with each other.This website is a platform for students to share their notes and study materials with each other.This website is a platform for students to share their notes and study materials with each other."];
+        $questions[1] = ["question" => "How can I upload my notes?", "answer" => "You can upload your notes by clicking on the 'Upload' button on the top right corner of the website."];
+        $questions[2] = ["question" => "How can I download notes?", "answer" => "You can download notes by clicking on the 'Download' button on the notes you want to download."];
+
+        $this->data["questions"] = $questions;
     }
 }
