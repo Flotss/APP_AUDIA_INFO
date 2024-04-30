85e28a2589936cffe14d8c9b65efd02058953a91
Walydd <waal63175@eleve.isep.fr>
2024-04-30 23:34:55 +0200

    cgu utlisateur 1

--- src/controller/CguController.php
+++ src/controller/CguController.php
@@ -10,5 +10,11 @@ class CguController extends AbstractController
     public function __construct()
     {
         parent::__construct("cgu");
+
+
+        // Recuperer le contenu depuis la base de donnée
+
+
+        //$this->data['cgu'] =  ta variable;
     }
 }
