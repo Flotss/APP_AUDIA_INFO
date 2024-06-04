f335336dbbab81b12fe7010db4f34c08f99bd605
Walydalami <waal63175@eleve.isep.fr>
2024-06-04 18:03:22 +0200

    j
    j

--- src/controller/CguAdminController.php
+++ src/controller/CguAdminController.php
@@ -28,6 +28,6 @@ class CguAdminController extends AbstractAdminController
         $service->updateContentByKey("cgu", $_POST['cgu']);
 
         $this->data['cgu'] = $_POST['cgu'];
-        $this->data['message'] = "CGU mis à jour";
+        $this->data['message'] = "CGU ont été mis à jour";
     }
 }
